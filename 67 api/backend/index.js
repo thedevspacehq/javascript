719f96998de94f18230a220ba6bb54e5555d975c
Eric Hu <huericnan@gmail.com>
2024-11-02 14:40:31 -0400

    fix cors

--- 67 api/backend/index.js
+++ 67 api/backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import postController from "../backend/controllers/postController.js";
 
+import cors from "cors";
+
 const app = express();
 const port = 3001;
+
+app.use(cors());
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
